a20143f062a0be8230cc1402b0d4017657553565
Daniel Gordon <daniel.gordon.0427@gmail.com>
2019-02-05 23:23:31 +0000

    all 4 motors in one thread, being controlled by main program

--- test/src/motor_main.cpp
+++ test/src/motor_main.cpp
@@ -7,134 +7,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-
-typedef enum endFlag {
-  START,
-  MID,
-  END
-} MotorFlag;
+#include <thread>
 
 typedef struct MotorPinLayout {
   int direction1, direction2, encoderA, encoderB;
   int strength, count, rpm;
-  MotorFlag flag;
 } MotorPins;
 
-void setupSoftMotor(int threadid, MotorPins &motorPin) {
-  Motor motor1(motorPin.direction1, motorPin.direction2);
-  int current = motorPin.strength;
-  while (motorPin.flag != END) {
-    if (motorPin.strength != current) {
-      current = motorPin.strength;
-      std::cout << "Thread " << threadid << " power " << current << std::endl;
-      if (current > 0 ) {
-        motor1.forward(current);
-      } else if (current < 0) {
-        motor1.backward((current*-1));
-      } else {
-        motor1.stop();
-      }
-    }
-  }
-  motor1.stop();
-}
-
-void setupHardMotor() {
-  Motor motor1 (23, 24, 25, HARDWARE);
-  printf("hard forward\n");
-  motor1.forward(1024);
-  delay(5000);
-  printf("stop\n");
-  motor1.stop();
-  delay(1000);
-  printf("backwards\n");
-  motor1.backward(1024);
-  delay(5000);
-  printf("done\n");
-  motor1.stop();
-}
-
-void IncrementEncoders(int Encodervalue, int &count, int &errCount) {
-  switch (Encodervalue) {
-    case 1:
-    case 7:
-    case 8:
-    case 14:
-    //forward direciton
-      count++;
-      break;
-    case 2:
-    case 4:
-    case 11:
-    case 13:
-    //reverse direction
-      count--;
-      break;
-    case 0:
-    case 5:
-    case 10:
-    case 15:
-      //no change
-      break;
-    case 3:
-    case 6:
-    case 9:
-    case 12:
-      //invalid change - what to do??
-      errCount++;
-  }
-}
-
-
-int main() {
-  // std::vector<Motor> motors;
-  std::vector<MotorPins> motorPins;
-  //motor1 1 setup
-  MotorPins motorPin1;
-  motorPin1.direction1 = 4;
-  motorPin1.direction2 = 5;
-  motorPin1.strength = 0;
-  motorPin1.encoderA = 6;
-  motorPin1.encoderB = 26;
-  motorPin1.flag = START;
-  motorPins.push_back(motorPin1);
-
-  //Motor 2 direction pins
-  MotorPins motorPin2;
-  motorPin2.direction1 = 28;
-  motorPin2.direction2 = 27;
-  motorPin2.strength = 0;
-  motorPin2.encoderA = 25;
-  motorPin2.encoderB = 29;
-  motorPin2.flag = START;
-  motorPins.push_back(motorPin2);
-
-  //Motor 3 direction pins
-  MotorPins motorPin3;
-  motorPin3.direction1 = 22;
-  motorPin3.direction2 = 21;
-  motorPin3.strength = 0;
-  motorPin3.encoderA = 24;
-  motorPin3.encoderB = 23;
-  motorPin3.flag = START;
-  motorPins.push_back(motorPin3);
-
-  //Motor 4 direction pins
-  MotorPins motorPin4;
-  motorPin4.direction1 = 7;
-  motorPin4.direction2 = 0;
-  motorPin4.strength = 0;
-  motorPin4.encoderA = 2;
-  motorPin4.encoderB = 3;
-  motorPin4.flag = START;
-  motorPins.push_back(motorPin4);
-
-  std::vector<Motor> motors;
-  motors.reserve(4);
-  for (unsigned i = 0; i < motorPins.size(); i++) {
-    motors.emplace_back(motorPins[i].direction1, motorPins[i].direction2, motorPins[i].encoderA, motorPins[i].encoderB);
-  }
-
+void motorThread(bool &end, std::vector<MotorPins> &motorPins, std::vector<Motor> &motors) {
   for (unsigned i = 0; i < motors.size(); i++) {
     motors[i].setOldA(motors[i].getEncoderA());
     motors[i].setOldB(motors[i].getEncoderB());
@@ -143,18 +23,12 @@ int main() {
   int Bin_Value = 0;
   float rpm_time = 0;
   int scale = 30*7*4;
-  int MotorPower = 30;
-
-  for (unsigned i = 0; i < motors.size(); i++) {
-    motors[i].forward(30);
-  }
 
   auto start = std::chrono::high_resolution_clock::now();
   auto finish = start;
   auto deltaT = start;
 
-  while (std::chrono::duration_cast<std::chrono::milliseconds>(finish-start).count() < 5000) {
-
+  while (!end) {
     for (unsigned i = 0; i < motors.size(); i++) {
       motors[i].setNewA(motors[i].getEncoderA());
       motors[i].setNewB(motors[i].getEncoderB());
@@ -163,21 +37,26 @@ int main() {
       motors[i].setOldA(motors[i].getNewA());
       motors[i].setOldB(motors[i].getNewB());
     }
-
     rpm_time = std::chrono::duration_cast<std::chrono::milliseconds>(finish - deltaT).count();
     if (rpm_time > 100) {
       for (unsigned i = 0; i < 4; i++) {
         float rpm = motors[i].getCount()/(rpm_time/1000 * scale)*60;
         motors[i].setRpm(rpm);
         motors[i].setCount(0);
-      }
-      if (motors[0].getRpm() > 50) {
-        MotorPower--;
-        motors[0].forward(MotorPower);
-      }
-      else if(motors[0].getRpm() < 50) {
-        MotorPower++;
-        motors[0].forward(MotorPower);
+        if (rpm > motorPins[i].rpm) {
+          motorPins[i].strength--;
+        }
+        else if(rpm < motorPins[i].rpm) {
+          motorPins[i].strength++;
+          motors[i].forward(motorPins[i].strength);
+        }
+        if (motorPins[i].strength > 0 ) {
+          motors[i].forward(motorPins[i].strength);
+        } else if (motorPins[i].strength < 0) {
+          motors[i].backward((motorPins[i].strength*-1));
+        } else {
+          motors[i].stop();
+        }
       }
      deltaT = std::chrono::high_resolution_clock::now();
     }
@@ -195,5 +74,75 @@ int main() {
     motors[i].stop();
   }
   delay(100);
+}
+
+int main() {
+  std::thread t;
+  std::vector<MotorPins> motorPins;
+  std::vector<Motor> motors;
+  bool end = false;
+  //motor1 1 setup
+  MotorPins motorPin1;
+  motorPin1.direction1 = 5;
+  motorPin1.direction2 = 4;
+  motorPin1.strength = 0;
+  motorPin1.encoderA = 26;
+  motorPin1.encoderB = 6;
+  motorPin1.rpm = -40;
+  motorPin1.strength = 0;
+  motorPins.push_back(motorPin1);
+
+  //Motor 2 direction pins
+  MotorPins motorPin2;
+  motorPin2.direction1 = 28;
+  motorPin2.direction2 = 27;
+  motorPin2.strength = 0;
+  motorPin2.encoderA = 25;
+  motorPin2.encoderB = 29;
+  motorPin2.rpm = -40;
+  motorPin2.strength = 0;
+  motorPins.push_back(motorPin2);
+
+  //Motor 3 direction pins
+  MotorPins motorPin3;
+  motorPin3.direction1 = 22;
+  motorPin3.direction2 = 21;
+  motorPin3.strength = 0;
+  motorPin3.encoderA = 24;
+  motorPin3.encoderB = 23;
+  motorPin3.rpm = -40;
+  motorPin3.strength = 0;
+  motorPins.push_back(motorPin3);
+
+  //Motor 4 direction pins
+  MotorPins motorPin4;
+  motorPin4.direction1 = 0;
+  motorPin4.direction2 = 7;
+  motorPin4.strength = 0;
+  motorPin4.encoderA = 3;
+  motorPin4.encoderB = 2;
+  motorPin4.rpm = -40;
+  motorPin4.strength = 0;
+  motorPins.push_back(motorPin4);
+
+  motors.reserve(4);
+  for (unsigned i = 0; i < motorPins.size(); i++) {
+    motors.emplace_back(motorPins[i].direction1, motorPins[i].direction2, motorPins[i].encoderA, motorPins[i].encoderB);
+  }
+
+  t = std::thread(motorThread, std::ref(end), std::ref(motorPins), std::ref(motors));
+  delay(5000);
+  for (unsigned i = 0; i < motors.size(); i++) {
+    motorPins[i].rpm = 0;
+  }
+  delay(2000);
+  for (unsigned i = 0; i < motors.size(); i++) {
+    motorPins[i].rpm = 50;
+  }
+  delay(5000);
+  end = true;
+  delay(1000);
+
+  t.detach();
   return 0;
 }
